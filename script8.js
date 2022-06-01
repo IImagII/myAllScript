42d7f65d41bbb58602915a65c7866bfde0e19d0f
iimagii <mag_1@ukr.net>
2022-06-01 21:41:11 +0300

    qwert

--- script8.js
+++ script8.js
@@ -71,10 +71,169 @@
 // console.log(user);
 // console.log(user.sayHello());
 // console.log(user.privet());
-export let one = 1;
-let two = 2;
-export { two };
-export function sayHello() {
-   console.log('Hello1');
-}
-console.log;
+// export let one = 1;
+// let two = 2;
+// export { two };
+// export function sayHello() {
+//    console.log('Hello1');
+// }
+// console.log;
+// async function f1() {
+//    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//    return res.text();
+// }
+// async function f2() {
+//    const tes = await fetch('https://jsonplaceholder.typicode.com/todos/2');
+//    return tes.text();
+// }
+
+// async function f3() {
+//    const res = await fetch('https://jsonplaceholder.typicode.com/todos/3');
+//    return res.text();
+// }
+
+// async function go() {
+//    let a = await f1();
+//    console.log(a);
+//    let b = await f2();
+//    console.log(b);
+//    let c = await f3();
+//    console.log(c);
+// }
+// go();
+// const usr = {
+//    name: 'Pupkin',
+//    profession: 'worked',
+//    salary: 10000,
+// };
+// // usr.passport = 'ss121222';
+// Object.defineProperty(usr, 'passport', {
+//    value: ' dd21212',
+//    enumerable: false,
+// });
+// let entries = Object.entries(usr);
+// entries.map(([index, item]) => console.log(index, item));
+
+// function other(c, y) {
+//    let d = c.reduce((acc, item) => acc + item / c.length);
+//    return d < y;
+// }
+// console.log(other([2, 3, 4], 5));
+
+// function isFun(string) {
+//    for (i = 0; i < string.length; i++) {
+//       const char = string[i];
+//       if (string.lastIndexOf(char) !== i) {
+//          return false;
+//       }
+//    }
+//    return true;
+// }
+
+// function isFun(string) {
+//    for (let i = 0; i < string.length; i++) {
+//       let char = string[i];
+//       if (string.lastIndexOf(char) !== i) {
+//          return false;
+//       }
+//    }
+
+//    return true;
+// }
+
+// function isFun(string) {
+//    string.forEach((item, i) => {
+//       let char = item;
+//       if (string.lastIndexOf(char) !== i) {
+//          return false;
+//       }
+//    });
+//    return true;
+// }
+
+// console.log(isFun('ascfVa'));
+
+// function flatten(arr) {
+//    const res = [];
+//    for (let i = 0; i < arr.length; i++) {
+//       if (Array.isArray(arr[i])) {
+//          let char = flatten(arr[i]);
+//          for (let j = 0; j < char.length; j++) {
+//             res.push(char[j]);
+//          }
+//       } else {
+//          res.push(arr[i]);
+//       }
+//    }
+
+//    return res;
+// }
+
+// function flatten(arr) {
+//    let res = [];
+//    arr.forEach((item) => {
+//       if (Array.isArray(item)) {
+//          let char = flatten(item);
+//          char.forEach((elem) => {
+//             res = [...res, elem];
+//          });
+//       } else {
+//          res = [...res, item];
+//       }
+//    });
+//    return res;
+// }
+
+// console.log(flatten([[1], [[2, 3]], [[[[4]]]]]));
+
+// function removeDup(str) {
+//    const res = [];
+//    const map = {};
+//    for (let i = 0; i < str.length; i++) {
+//       const char = str[i];
+//       if (!map[char]) {
+//          map[char] = true;
+//          res.push(char);
+//       }
+//    }
+
+//    return res.join('');
+// }
+
+// console.log(removeDup('acbcbda'));
+
+// function higthFreg(arr) {
+//    const map = {};
+//    let maxFreg = 0;
+//    let maxFregmax = arr[0];
+//    arr.forEach((item) => {
+//       if (map[item]) {
+//          map[item]++;
+//       } else {
+//          map[item] = 1;
+//       }
+//       if (map[item] > maxFreg) {
+//          maxFreg = map[item];
+//          maxFregmax = item;
+//       }
+//    });
+//    return maxFregmax;
+// }
+
+// console.log(higthFreg(['edc', 'abc', 'dfc', 'abc', 'fgg']));
+// const START = Date.now();
+
+// function someFn() {
+//    console.log('time', Date.now() - START);
+//    console.log('args', arguments);
+// }
+
+// Function.prototype.delay = function (ms) {
+//    return (...args) => {
+//       setTimeout(() => {
+//          this.call(this, ...args);
+//       }, ms);
+//    };
+// };
+
+// const f = someFn.delay(5000, 200, 300);
