1bc669fc44d0a30a92a61836415e0686b39e9fe7
iimagii <mag_1@ukr.net>
2022-07-02 17:38:01 +0300

    fetch

--- script9.js
+++ script9.js
@@ -1,17 +1,27 @@
-let myRequest = new XMLHttpRequest();
-let a = 0;
+// let myRequest = new XMLHttpRequest();
+// let a = 0;
 
-myRequest.addEventListener('readystatechange', function () {
-   if (this.readyState == 4 && this.status == 200) {
-      myFunction(this.responseText);
-   }
-});
+// myRequest.addEventListener('readystatechange', function () {
+//    if (this.readyState == 4 && this.status == 200) {
+//       myFunction(this.responseText);
+//    }
+// });
 
-myRequest.open('GET', 'http://getpost.itgid.info/index2.php', true);
-myRequest.send();
+// myRequest.open('GET', 'http://getpost.itgid.info/index2.php', true);
+// myRequest.send();
 
-function myFunction(data) {
-   a = data;
-   console.log(data);
-}
-console.log(a);
+// function myFunction(data) {
+//    a = data;
+//    console.log(data);
+// }
+// console.log(a);
+
+fetch('http://getpost.itgid.info/index2.php', {
+   method: 'GET',
+   headers: {
+      //  'Content-Type': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+   },
+})
+   .then((response) => response.text())
+   .then((response) => console.log(response));
